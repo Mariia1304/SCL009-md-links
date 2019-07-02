e51a03a504d7631d810ed6339c54974297293326
Mariia <maria.bookvoed@gmail.com>
2019-07-01 22:52:03 -0400

    optimizar funcion md-links, ahora si es promise, quitar codigo muerto

--- src/md-links.js
+++ src/md-links.js
@@ -4,26 +4,37 @@ const fs = require('fs');
 const fileHound = require('filehound');
 const fetch = require('node-fetch');
 
+const isDirectory = (path) => {
+     return new Promise ((resolve, reject) => {
+          fs.lstat(path, (err, stats) => {
+               if(err){
+                    if(err.code == 'ENOENT'){
+                         reject(console.log("ruta no valida"));
+               }
+               } else if (stats.isDirectory()){
+                    extractMdFiles(path)
+                         .then(res=>{
+                              resolve(res)
+                         })
+                    .catch(err => {
+                         reject(err)
+                    })
+         
+               } else {
+                    links(path)
+                         .then(res=>{
+                              resolve(res)
+                         })
+                         .catch(err => {
+                         reject(err)
+                         })
+               }
+          })
+     })
+};
 
 
-// //funcion para saber la ruta es archivo o directorio
-// const isDirectory = async path => {
-//   try {
-//     return (await util.promisify(fs.lstat)(path)).isDirectory()
-//   } catch (e) {
-//       console.log(e);
-//     return false // or custom the error
-//   }
-// }
-function is_dir(path) {
-     try {
-         var stat = fs.lstatSync(path);
-         return stat.isDirectory();
-     } catch (e) {
-         // lstatSync throws an error if path doesn't exist
-         return false;
-     }
- }
+
 // funcion que lee archivo y extrae los links como un array de objetos
 const links = (path) =>{
   if(pathNode.extname(path) != ".md"){
@@ -44,7 +55,6 @@ const links = (path) =>{
                            }) 
                       }
                       marked(data, {renderer:renderer});
-                 
                       resolve(links);                                                                   
                  }
             })     
@@ -56,127 +66,140 @@ const links = (path) =>{
 
 // funcion para encontrar y extraer archivos con extencion .md de un directorio
 const extractMdFiles = (path) =>{
-  return new Promise((resolve, reject)=>{
-       fileHound.create()
-       .paths(path)
-       .ext('md') 
-       .find()
-       .then(mdFiles=>{
-           mdFiles.forEach(el => {
-                resolve(links(el))
-           })
-            //resolve(links(mdFiles))
-       })
-       .catch(err => {
-            reject(err)
-       })
-  })
-
-}
-// mostrar en la consola contenido de cada link sin opciones
-const printLinks = (links)=>{
-  links.map(link=>{
-       console.log(link.href, link.file, link.text);
-  })
-}
-// obtener datos de fetch y agregarlos en links para usar despues en validate y stats
-const linksToStatsAndValidate = (links)=>{
-  return Promise.all(links.map(link=>{
-       return new Promise((resolve, reject)=>{
-            fetch(link.href)
-                 .then(res=>{
-                      link.statusCode = res.status;
-                      link.statusText = res.statusText;                 
-                      resolve(link)
-                     
-                 })
-                 .catch((err)=>{
-                      link.statusCode = 0;
-                      link.statusText = err.code;
-                      resolve(link)
-                     
-                 })
-       })
-  }))
- 
-}  
-// opcion stats para contar y mostrar en consola links unicos y totales
-const statsLinks = (links) =>{
-  const hrefLinks = links.map(el=>el.href);
-  let linksTotal = hrefLinks.length;
-  console.log("Links Totales: ",linksTotal);
-  const uniqueLinks = [new Set(hrefLinks)].length;
-  console.log("Links Unicos: ",uniqueLinks);
-} 
-// opcion validate para mostrar en consola datos completos sobre link
-const validateLinks = (links)=>{
-     links.map(link=>{
-          console.log(link.text, link.href, link.statusCode, link.statusText, link.file)
-     })
-}
-// contar y mostrar links unicos totales y ademas de eso rotos(malos)
-const validateAndStatsLinks = (links)=>{
-   let linksBroken = links.filter(link=>{
-        return link.statusCode < 200 || link.statusCode > 400
-   });
-   statsLinks(links);
-   console.log("Links Rotos: ",linksBroken.length)
-  
-}
-
-// funcion mdLinks para conectar todas las funciones aunque todavia no es promesa, voy por allí
-const mdLinks = (path, options) => {
      return new Promise((resolve, reject)=>{
-          if(is_dir(path)){
-               extractMdFiles(path)
-                    .then((links)=>{
-                         if(options.stats&&options.validate){
-                              linksToStatsAndValidate(links)
-                                   .then(res=>{
-                                        resolve(validateAndStatsLinks(res) )                                                                                
-                                        })
-                         }else if(options.stats){
-                              statsLinks(links)
-                         }else if(options.validate){
-                              linksToStatsAndValidate(links)
-                                   .then(res =>{
-                                        resolve(validateLinks(res))
-                                   })
-                         }else(
-                              resolve(printLinks(links))
-                         )                                                    
-                    })
-                    .catch(err=>{
-                         rconsole.log(err)
-                         })
-          }else{
-               links(path)
-                    .then((links)=>{
-                         if(options.stats&&options.validate){
-                              linksToStatsAndValidate(links)
-                                   .then(res=>{
-                                        resolve(validateAndStatsLinks(res))                                                                                                     
-                                        })
-                         }else if(options.stats){
-                             resolve(statsLinks(links)) 
-                         }else if(options.validate){
-                              linksToStatsAndValidate(links)
-                                   .then(res =>{
-                                        resolve(validateLinks(res))
-                                   })
+          fileHound.create()
+          .paths(path)
+          .ext('md') 
+          .find()
+          .then(mdFiles=>{
+               mdFiles.forEach(el => {
+                    resolve(links(el))
+               })
+               
+          })
+          .catch(err => {
+               reject(err)
+          })
+     })
+
+}
+
+const linksToStatsAndValidate = (links)=>{
+     return Promise.all(links.map(link=>{
+          return new Promise((resolve, reject)=>{
+               fetch(link.href)
+                    .then(res=>{
+                         if(res.status<200||res.status>400){
+                              link.statusCode = res.status;
+                              link.statusText = "fail"; 
+                              resolve(link)  
                          }else{
-                              resolve(printLinks(links))
+                              link.statusCode = res.status;
+                              link.statusText = res.statusText;                 
+                              resolve(link)
                          }
                     })
-                    .catch(err=>{
-                         reject(err)
-                         })                                  
-               }
-     })   
+                    .catch((err)=>{
+                         if(err){
+                              link.statusCode = 0;
+                              link.statusText = "fail";
+                              resolve(link)
+                         }
+                    })
+          })
+     }))
+ 
+} 
+const callLinksToStatsAndValidate = (path)=>{
+     return new Promise ((resolve, reject) => {
+          isDirectory(path)
+               .then(res=>{
+                    linksToStatsAndValidate(res)
+                         .then(res=>{
+                              resolve(res)
+                         })
+                         .catch(err => {
+                              reject(err)
+                         })
+               })
+               .catch(err => {
+                    reject(err)
+               })
+     })
+}
+// opcion stats para contar y mostrar en consola links unicos y totales
+const statsLinks = (links) =>{
+     return new Promise((resolve, reject)=>{
+          const linksHref = links.map(el=>el.href);
+          let totalLinks = linksHref.length;
+          const uniqueLinks = [new Set(linksHref)].length;
+          resolve(`Links Totales: ${totalLinks},\nLinks Unicos: ${uniqueLinks}`)
+     })
      
-         
+   } 
+   // opcion validate para mostrar en consola datos completos sobre link
+   const validateLinks = (links)=>{
+        return new Promise((resolve, reject)=>{
+            let linksValidate = links.map(link=>`\n${link.file} ${link.href} ${link.statusCode} ${link.statusText} ${link.text}`);
+            resolve(`${linksValidate}`)
+    
+           
+        })
+   }
+   // contar y mostrar links unicos totales y ademas de eso rotos(malos)
+const validateAndStatsLinks = (links)=>{
+     return new Promise((resolve, reject)=>{
+          let linksTotal = links.map(link=>link.href);
+          let linksTotales = linksTotal.length;
+          let linksUnique = [new Set(linksTotal)].length;
+          let linksBroken = links.filter(link=>{
+               link.statusCode < 200 || link.statusCode > 400
+               });
+          linksBroken=linksBroken.length;
+          
+          resolve(`Links Totales: ${linksTotales},\nLinks Unicos: ${linksUnique},\nLinks Rotos: ${linksBroken}`)
+     })
+      
+}
+   
+
+const mdLinks = (path, options) => {
+     return new Promise ((resolve, reject) => {
+          callLinksToStatsAndValidate(path)
+               .then(res=>{
+                    if(options.validate&&options.stats){
+                         validateAndStatsLinks(res)
+                              .then(res=>{
+                                   resolve(res)
+                              })
+                              .catch(err=>{
+                                   reject(err)
+                              })
+                    }else if(options.validate===true&&options.stats===false){
+                         validateLinks(res)
+                              .then(res=>{
+                                   resolve(res)
+                              })
+                              .catch(err=>{
+                                   reject(err)
+                              })
+                    }else if(options.stats===true&&options.validate===false){
+                         statsLinks(res)
+                              .then(res=>{
+                                   resolve(res)
+                              })
+                              .catch(err=>{
+                                   reject(err)
+                              })
+                    }
+               })
+      
+     })
 }
 //exportar funcion md-links
 module.exports = {
   mdLinks
 }
+
+
+
