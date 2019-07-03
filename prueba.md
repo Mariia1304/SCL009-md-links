c3fd316342b3d5bae1b25df0a987ed45d6add665
Mariia <maria.bookvoed@gmail.com>
2019-07-02 22:46:17 -0400

    error en leer directorio(agarra solamente primer archivo .md)

--- prueba.md
+++ prueba.md
@@ -2,3 +2,6 @@ hola mundo!
 
 - [Acerca de Node.js - Documentación oficial](https://nodejs.org/es/about/)
 - [Node.js file system - Documentación oficial](https://nodejs.org/api/fs.html)
+- [Node.js file system - Documentación oficial](https://nodejs.org/api/fs.html)
+- [Acerca de Node.js - Documentación oficial](https://nodejs.org/es/about/)
+
--- src/md-links.js
+++ src/md-links.js
@@ -3,85 +3,95 @@ const marked = require('marked');
 const fs = require('fs');
 const fileHound = require('filehound');
 const fetch = require('node-fetch');
+const colors = require('colors')
 
 const isDirectory = (path) => {
      return new Promise ((resolve, reject) => {
           fs.lstat(path, (err, stats) => {
                if(err){
                     if(err.code == 'ENOENT'){
-                         reject(console.log("ruta no valida"));
-               }
+                         reject("ruta no valida")
+                    }
                } else if (stats.isDirectory()){
                     extractMdFiles(path)
-                         .then(res=>{
-                              resolve(res)
-                         })
-                    .catch(err => {
-                         reject(err)
-                    })
-         
+                         .then((res)=>{
+                              console.log(res)
+                              Promise.all(res)
+                                   .then(res=>res.map(e=>{
+                                        console.log(e)
+                                        resolve(links(e))
+                                   }))
+                              })
+                                   // console.log(links)
+                                   // resolve(links)
+                        
+                              // res.map((res)=>{
+                              //      links(res)
+                              //           .then(resultLinks => {
+                              //                linksToStatsAndValidate(resultLinks)
+                              //                     .then(resultLinksToStatsAndValidate => {
+                              //                          resolve(resultLinksToStatsAndValidate)
+                              //                     })
+                              //                // console.log("RESPONSE extractMdFiles.res.map", resultLinks);
+                              //           })
+                              // })
+                     
                } else {
                     links(path)
                          .then(res=>{
                               resolve(res)
                          })
                          .catch(err => {
-                         reject(err)
+                              reject(err)
                          })
                }
           })
      })
-};
+}
 
 
 
 // funcion que lee archivo y extrae los links como un array de objetos
 const links = (path) =>{
-  if(pathNode.extname(path) != ".md"){
-     console.log("Es archivo pero no .md")  
-  }else{
-       return new Promise((resolve,reject)=>{         
-            fs.readFile(path,"utf-8", (err,data) =>{
-                 if(err){ 
-                      reject(err); 
-                 }else{
-                      let links =[];   
-                      const renderer = new marked.Renderer();
-                      renderer.link = function(href, title, text){
-                           links.push({
-                                href:href,
-                                text:text,
-                                file:path
-                           }) 
-                      }
-                      marked(data, {renderer:renderer});
-                      resolve(links);                                                                   
-                 }
-            })     
-       
-       })
-  }
-       
+    return new Promise((resolve,reject)=>{
+        try{
+            if(pathNode.extname(path)!=".md"){
+                throw(new Error("Extensión no válida"));
+            }
+            fs.readFile(path,'utf-8',(err, content)=>{
+                if(err){
+                    reject(err.code);
+                }
+                else{
+                    let links=[];
+                    const renderer = new marked.Renderer();
+                    renderer.link = function(href, title, text){
+                        links.push({
+                            href:href, 
+                            text: text,
+                            file: path
+                        })
+                    }
+                    marked(content,{renderer:renderer});
+                    resolve(links);
+                }
+            })  
+        }
+        catch(error){
+            reject(error);
+        }        
+    })
 }
-
 // funcion para encontrar y extraer archivos con extencion .md de un directorio
 const extractMdFiles = (path) =>{
      return new Promise((resolve, reject)=>{
-          fileHound.create()
+          let mdFiles = fileHound.create()
           .paths(path)
           .ext('md') 
           .find()
-          .then(mdFiles=>{
-               mdFiles.forEach(el => {
-                    resolve(links(el))
-               })
-               
-          })
-          .catch(err => {
-               reject(err)
-          })
-     })
+          resolve(mdFiles)
 
+     })
 }
 
 const linksToStatsAndValidate = (links)=>{
@@ -110,28 +120,28 @@ const linksToStatsAndValidate = (links)=>{
      }))
  
 } 
-const callLinksToStatsAndValidate = (path)=>{
-     return new Promise ((resolve, reject) => {
-          isDirectory(path)
-               .then(res=>{
-                    linksToStatsAndValidate(res)
-                         .then(res=>{
-                              resolve(res)
-                         })
-                         .catch(err => {
-                              reject(err)
-                         })
-               })
-               .catch(err => {
-                    reject(err)
-               })
-     })
-}
+// const callLinksToStatsAndValidate = (path)=>{
+//      return new Promise ((resolve, reject) => {
+//           isDirectory(path)
+//                .then(res=>{
+//                     linksToStatsAndValidate(res)
+//                          .then(res=>{
+//                               resolve(res)
+//                          })
+//                          .catch(err => {
+//                               reject(err)
+//                          })
+//                })
+//                .catch(err => {
+//                     reject(err)
+//                })
+//      })
+// }
 //mostrar la informacion sobre links en caso si usuario no pusa opcion stats o validate
 const simpleLinks = (links)=>{
      return new Promise((resolve, reject)=>{
           let linksValidate = links.map(link=>`\n${link.file} ${link.href} ${link.text}`);
-            resolve(`${linksValidate}`)
+            resolve(`${linksValidate}`.green)
      })
 }
 // opcion stats para contar y mostrar en consola links unicos y totales
@@ -139,18 +149,15 @@ const statsLinks = (links) =>{
      return new Promise((resolve, reject)=>{
           const linksHref = links.map(el=>el.href);
           let totalLinks = linksHref.length;
-          const uniqueLinks = [new Set(linksHref)].length;
-          resolve(`Links Totales: ${totalLinks},\nLinks Unicos: ${uniqueLinks}`)
+          let uniqueLinks = [...new Set(linksHref)].length;
+          resolve(`Links Totales:${totalLinks},\nLinks Unicos: ${uniqueLinks}`)
      })
-     
-   } 
+} 
 // opcion validate para mostrar en consola datos completos sobre link
 const validateLinks = (links)=>{
      return new Promise((resolve, reject)=>{
           let linksValidate = links.map(link=>`\n${link.file} ${link.href} ${link.statusCode} ${link.statusText} ${link.text}`);
           resolve(`${linksValidate}`)
-
-          
      })
 }
 // contar y mostrar links unicos totales y tambien los rotos(malos)
@@ -158,52 +165,45 @@ const validateAndStatsLinks = (links)=>{
      return new Promise((resolve, reject)=>{
           let linksTotal = links.map(link=>link.href);
           let linksTotales = linksTotal.length;
-          let linksUnique = [new Set(linksTotal)].length;
+          let linksUnique = [...new Set(linksTotal)].length;
           let linksBroken = links.filter(link=>{
                link.statusCode < 200 || link.statusCode > 400
                });
           linksBroken=linksBroken.length;
-          
           resolve(`Links Totales: ${linksTotales},\nLinks Unicos: ${linksUnique},\nLinks Rotos: ${linksBroken}`)
      })
-      
 }
-   
 
 const mdLinks = (path, options) => {
      return new Promise ((resolve, reject) => {
-          callLinksToStatsAndValidate(path)
-               .then(res=>{
-                    if(options.validate&&options.stats){
-                         validateAndStatsLinks(res)
-                              .then(res=>{
-                                   resolve(res)
-                              })
-                             
-                    }else if(options.validate){
-                         validateLinks(res)
-                              .then(res=>{
-                                   resolve(res)
-                              })
-                             
-                    }else if(options.stats){
-                         statsLinks(res)
-                              .then(res=>{
-                                   resolve(res)
-                              })
-                             
-                    }else{
-                         simpleLinks(res)
-                              .then(res=>{
-                                   resolve(res)
-                              })
-                              
-                    }
+          extractMdFiles(path)
+               .then((mdFiles)=>{
+                    resolve(mdFiles)
                })
-               .catch(err=>{
-                    reject(err)
+               .catch(()=>{
+                    links(path)
+                         .then((links)=>{
+                              linksToStatsAndValidate(links)
+                                   .then((links)=>{
+                                        if(options.validate&&options.stats){
+                                             resolve(validateAndStatsLinks(links))
+                                                 
+                                        }else if(options.validate){
+                                             resolve(validateLinks(links))
+                                                
+                                        }else if(options.stats){
+                                             resolve(statsLinks(links))
+                                                  
+                                                 
+                                        }else{
+                                             resolve(simpleLinks(links))
+                                             
+                                        }
+                                       
+                                   })
+                                   
+                         })
                })
-      
      })
 }
 //exportar funcion md-links
